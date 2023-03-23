448ef904fd6601681bffc80ebfddcbce4f4736dc
unknown <oscar.cruzgarcia@ihg.com>
2023-03-23 18:20:13 +0000

    Docker and terraform files

---
+++ terraform-iac/s3.tf
@@ -0,0 +1,36 @@
+resource "aws_s3_bucket" "this" {
+        bucket_prefix = "mvws9-cruz-o"
+        force_destroy = true
+
+    tags = {
+            Name = "multiverse"
+    }
+}
+
+resource "aws_s3_bucket_acl" "this" {
+    bucket = aws_s3_bucket.this.id
+    acl = "private"
+}
+
+resource "aws_s3_object" "this" {
+    bucket = aws_s3_bucket.this.id
+    key = "results.csv"
+    source = "${path.module}/results.csv"
+    etag = filemd5("${path.module}/results.csv")
+}
+
+# Zip the code
+data "archive_file" "source" {
+  type        = "zip"
+  source_dir  = "../python-survey-app"
+  output_path = "../code.zip"
+}
+
+#upload the code
+resource "aws_s3_object" "code" {
+    bucket = aws_s3_bucket.this.id
+    key = "code.zip"
+    source = "${data.archive_file.source.output_path}" # its mean it depended on zip
+    etag = filemd5("${data.archive_file.source.output_path}")
+}
+
